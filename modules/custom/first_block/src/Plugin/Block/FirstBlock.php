4115aae92abe395e90aeb9434e3aeafd900e67be
Vlad <meroosever@mail.ru>
2018-02-28 14:57:22 +0300

    Добавление перевода полей в модуле first_block

--- modules/custom/first_block/src/Plugin/Block/FirstBlock.php
+++ modules/custom/first_block/src/Plugin/Block/FirstBlock.php
@@ -21,9 +21,9 @@ use Drupal\file\Entity\File;
  */
 class FirstBlock extends BlockBase {
 
-    /**
-     * {@inheritdoc}
-     */
+  /**
+   * {@inheritdoc}
+   */
   public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
     //$config = $this->getConfiguration();
@@ -32,15 +32,16 @@ class FirstBlock extends BlockBase {
 
     $form['textfield'] = [
       '#type' => 'textfield',
-      '#title' => t('title'),
+      '#title' => t('My textfield'),
       //'#default_value' => $config['textfield'],
     ];
 
     $form['image'] = [
       '#type' => 'managed_file',
-      '#title' => t('File *'),
+      '#title' => t('File'),
       '#size' => 20,
       '#description' => t('PDF format only'),
+      '#required' => true,
       '#upload_validators' => [
         'file_validate_extensions' => [
           'jpg',
@@ -56,7 +57,7 @@ class FirstBlock extends BlockBase {
 
     $form['textarea'] = [
       '#type' => 'textarea',
-      '#title' => t('textarea'),
+      '#title' => t('My textarea'),
       //'#default_value' => $config['textarea'],
     ];
 
@@ -66,104 +67,99 @@ class FirstBlock extends BlockBase {
       //'#default_value' => $config['date'],
     ];
 
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => t('Submit'),
-    ];
-
     return $form;
   }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function blockValidate($form, FormStateInterface $form_state)
-    {
-      $textfield_count = $form_state->getValue('textfield_count');
-      $textarea_count = $form_state->getValue('textarea_count');
+  /**
+   * {@inheritdoc}
+   */
+  public function blockValidate($form, FormStateInterface $form_state)
+  {
+    $textfield_count = $form_state->getValue('textfield_count');
+    $textarea_count = $form_state->getValue('textarea_count');
 
-      if(strlen($textfield_count) > 50) {
-        $form_state->setErrorByName('textfield',t('This field can not contain more than 50 characters.'));
-      }
-
-      if(strlen($textarea_count) > 255) {
-        $form_state->setErrorByName('textarea',t('This field can not contain more than 255 characters.'));
-      }
+    if(strlen($textfield_count) > 50) {
+      $form_state->setErrorByName('textfield',t('This field can not contain more than 50 characters.'));
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function blockSubmit($form, FormStateInterface $form_state)
-    {
-        $this->configuration['textfield'] = $form_state->getValue('textfield');
-        $this->configuration['image'] = $form_state->getValue('image');
-        $this->configuration['textarea'] = $form_state->getValue('textarea');
-        $this->configuration['date'] = $form_state->getValue('date');
+    if(strlen($textarea_count) > 255) {
+      $form_state->setErrorByName('textarea',t('This field can not contain more than 255 characters.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state)
+  {
+    $this->configuration['textfield'] = $form_state->getValue('textfield');
+    $this->configuration['image'] = $form_state->getValue('image');
+    $this->configuration['textarea'] = $form_state->getValue('textarea');
+    $this->configuration['date'] = $form_state->getValue('date');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+
+    $config = $this->getConfiguration();
+    $textarea = $config['textarea'];
+    $image_id = $config['image'][0];
+    $textfield = $config['textfield'];
+    $date = $config['date'];
+    $file = File::load($image_id);
+
+
+    $variables = array(
+      'style_name' => 'tes_',
+      'uri' => $file -> getFileUri(),
+    );
+
+    // The image.factory service will check if our image is valid.
+    $image = \Drupal::service('image.factory')->get($file->getFileUri());
+    if ($image -> isValid()) {
+      $variables['width'] = $image -> getWidth();
+      $variables['height'] = $image -> getHeight();
+    }
+    else {
+      $variables['width'] = $variables['height'] = NULL;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function build()
-    {
+    $logo_render_array = [
+      '#theme' => 'image_style',
+      '#width' => $variables['width'],
+      '#height' => $variables['height'],
+      '#style_name' => $variables['style_name'],
+      '#uri' => $variables['uri'],
+    ];
 
-      $config = $this->getConfiguration();
-      $textarea = $config['textarea'];
-      $image_id = $config['image'][0];
-      $textfield = $config['textfield'];
-      $date = $config['date'];
-      $file = File::load($image_id);
+    $renderer = \Drupal::service('renderer');
 
+    $block = array();
 
-      $variables = array(
-        'style_name' => 'tes_',
-        'uri' => $file -> getFileUri(),
-      );
+    $block['textarea'] = [
+      '#type' => 'markup',
+      '#markup' => t("$textarea"),
+    ];
 
-      // The image.factory service will check if our image is valid.
-      $image = \Drupal::service('image.factory')->get($file->getFileUri());
-      if ($image -> isValid()) {
-        $variables['width'] = $image -> getWidth();
-        $variables['height'] = $image -> getHeight();
-      }
-      else {
-        $variables['width'] = $variables['height'] = NULL;
-      }
+    $block['image'] = [
+      '#type' => 'markup',
+      '#markup' => $renderer->render($logo_render_array),
+    ];
 
-      $logo_render_array = [
-        '#theme' => 'image_style',
-        '#width' => $variables['width'],
-        '#height' => $variables['height'],
-        '#style_name' => $variables['style_name'],
-        '#uri' => $variables['uri'],
-      ];
+    $block['textfield'] = [
+      '#type' => 'markup',
+      '#markup' => t("$textfield"),
+    ];
 
-      $renderer = \Drupal::service('renderer');
+    $block['date'] = [
+      '#type' => 'markup',
+      '#markup' => t("<br>"."$date"),
+    ];
 
-      $block = array();
-
-      $block['textarea'] = [
-        '#type' => 'markup',
-        '#markup' => t("$textarea"),
-      ];
-
-      $block['image'] = [
-        '#type' => 'markup',
-        '#markup' => $renderer->render($logo_render_array),
-      ];
-
-      $block['textfield'] = [
-        '#type' => 'markup',
-        '#markup' => t("$textfield"),
-      ];
-
-      $block['date'] = [
-        '#type' => 'markup',
-        '#markup' => t("<br>"."$date"),
-      ];
-
-      return $block;
-    }
+    return $block;
+  }
 
 }
